c2832e77892e394d736b6d733691bd9379409276
Gauresh-bamane <63413571+Gauresh-bamane@users.noreply.github.com>
2022-08-28 22:48:58 +1000

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
-
 import serial
 import time
 import paho.mqtt.client as mqtt
 import string
+
+
+ser = serial.Serial("/dev/rfcomm0", 9600)
+ser.write(str.encode('Start\r\n'))
 
 def on_connect(client, userdata, flags, rc): # func for making connection
 	print("Connected to MQTT")
@@ -10,16 +13,18 @@ def on_connect(client, userdata, flags, rc): # func for making connection
 	
 	client.subscribe("ifn649")
 def on_message(client, userdata, msg): # Func for Sending msg
-	s=''.join(map(chr,msg.payload))
+	s="".join(map(chr,msg.payload))
 	m= float(s)
-	if m >= 30:
-		print("LED STATUS: ON"+"|"+"TEMPRATURE is : "+ s +"C")
-		ser.write(str.encode('LLED_ON'))
-	else:
-		print("LED STATUS: OFF"+ "|" + "TEMPRATURE is :" + s + "C")
-		ser.write(str.encode('LLED_OFF'))
+	switch(m, s)
 	
-
+def switch(m,s):
+    if m < 60:
+        print("Led status: ON"+"|"+"Humidity is : "+ s +"C")
+        ser.write(str.encode('LLED_ON'))
+    else:
+        print("Led status: OFF"+ "|" + "Humidity is :" + s + "C")
+        ser.write(str.encode('LLED_OFF'))
+        
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
@@ -27,3 +32,4 @@ client.on_message = on_message
 client.connect("13.54.211.159", 1883, 60)
 
 client.loop_forever()
+
